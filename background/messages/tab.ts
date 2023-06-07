f5b77774cedf5a3fde3a4ee0a0b072053e2b2bf2
siegerts <stephen.siegert@gmail.com>
2023-06-06 22:34:31 -0400

    return title from tab

--- background/messages/tab.ts
+++ background/messages/tab.ts
@@ -2,7 +2,8 @@ import type { PlasmoMessaging } from "@plasmohq/messaging"
 
 const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
   return res.send({
-    url: req.sender.tab.url
+    url: req.sender.tab.url,
+    title: req.sender.tab.title
   })
 }
 
